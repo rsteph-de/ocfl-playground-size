d7c904a1b0fc8c4f47a2f042abf9e8014517e092
Robert Stephan <robert.stephan@uni-rostock.de>
2024-08-26 17:40:25 +0200

    work with new ocfl-java api version

--- src/main/java/edu/example/ocfl/Main.java
+++ src/main/java/edu/example/ocfl/Main.java
@@ -1,10 +1,10 @@
 package edu.example.ocfl;
-import edu.example.ocfl.size_as_fixity.OCFLTestWithSizeAsFixity;
+import edu.example.ocfl.size_as_fixity.OCFLTestWithSizeNew;
 
 public class Main {
 
     public static void main(String[] args) {
-        OCFLTestWithSizeAsFixity app = new OCFLTestWithSizeAsFixity();
+        OCFLTestWithSizeNew app = new OCFLTestWithSizeNew();
         app.run();
 
     }
--- src/main/java/edu/example/ocfl/size_as_fixity/OCFLTestWithSizeNew.java
+++ src/main/java/edu/example/ocfl/size_as_fixity/OCFLTestWithSizeNew.java
@@ -1,4 +1,5 @@
 package edu.example.ocfl.size_as_fixity;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -6,8 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
 
-import edu.example.ocfl.size_as_fixity.size_digest.SizeDigestAlgorithm;
-import edu.example.ocfl.size_as_fixity.size_digest.SizeMessageDigest;
+import io.ocfl.api.DigestAlgorithmRegistry;
 import io.ocfl.api.OcflRepository;
 import io.ocfl.api.model.ObjectVersionId;
 import io.ocfl.api.model.OcflObjectVersion;
@@ -16,7 +16,7 @@ import io.ocfl.api.model.VersionInfo;
 import io.ocfl.core.OcflRepositoryBuilder;
 import io.ocfl.core.extension.storage.layout.config.HashedNTupleLayoutConfig;
 
-public class OCFLTestWithSizeAsFixity {
+public class OCFLTestWithSizeNew {
 
     public void run() {
         Path repoDir = Paths.get("c:\\Temp\\ocfl-test\\ocfl-repo"); // This directory contains the OCFL storage root.
@@ -43,27 +43,22 @@ public class OCFLTestWithSizeAsFixity {
                 new VersionInfo().setMessage("initial commit"));
 
             //retrieve size from File
-            String s = SizeMessageDigest.formatHex(Files.size(sampleFile2));
+            String s = String.valueOf(Files.size(sampleFile2));
 
-            //example for use of sizeMessageDigest
-            SizeMessageDigest sizeDigest = new SizeMessageDigest();
-            sizeDigest.update(Files.readAllBytes(sampleFile2));
-            String sizeResult = SizeMessageDigest.formatHex(sizeDigest.digest());
-
-            System.out.println("Size: from file: " + s + " / from digest algorithm: " + sizeResult);
+            System.out.println("Size: from file: " + s);
 
             //add new files to OCFL object (with size as fixity)
             repo.updateObject(ObjectVersionId.head("o1"), new VersionInfo().setMessage("update"), updater -> {
                 updater.addPath(sampleFile2, "bagit/rfc8493.txt.pdf")
-                    .addFileFixity("bagit/rfc8493.txt.pdf", new SizeDigestAlgorithm(), s)
+                    .addFileFixity("bagit/rfc8493.txt.pdf", DigestAlgorithmRegistry.getAlgorithm("size"), s)
                     .addPath(sampleFile2, "bagit__copy/rfc8493.txt.pdf")
-                    .addFileFixity("bagit__copy/rfc8493.txt.pdf", new SizeDigestAlgorithm(), s);
+                    .addFileFixity("bagit__copy/rfc8493.txt.pdf", DigestAlgorithmRegistry.getAlgorithm("size"), s);
             });
 
             //retrieve size from OCFL repo
             OcflObjectVersion obj = repo.getObject(ObjectVersionId.head("o1"));
             OcflObjectVersionFile file = obj.getFile("bagit__copy/rfc8493.txt.pdf");
-            String sizeOut = file.getFixity().get(new SizeDigestAlgorithm());
+            String sizeOut = file.getFixity().get(DigestAlgorithmRegistry.getAlgorithm("size"));
             System.out.println("Size: from OCFL: " + sizeOut);
 
         } catch (IOException e) {
